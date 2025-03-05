fd35fc24a69080ab7a69d6115d92305a4d5a6f2f
Josué Hernández <joheca1998@outlook.com>
2025-03-04 18:07:57 -0600

    Aplicando Render Props y Composition

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,20 @@
-export const ParentComponent = () => {
+import React, { JSX } from 'react';
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+}
+
+export const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name = "Josué Hernández"
   return (
     <>
+      {render(name)}
     </>
+  );
+};
+
+export const ParentComponent = () => {
+  return (
+    <ChildComponent render={(name) => <p>Hola, {name}!</p>}/>
   );
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,16 @@
-const ParentComponent = () => {
-};
+import { useState } from "react";
+
+interface ParenComponentProps {
+  render: (data:string[]) => React.ReactNode;
+}
+
+const ParentComponent: React.FC<ParenComponentProps>= ({ render }) => {
+  const [data] = useState<string[]>(["Apple 🍎", "Banana 🍌", "Orange 🍊"]);
+  
+  return(
+    <ul>{render(data)}</ul>
+  )
+  
+}
 
 export default ParentComponent;
