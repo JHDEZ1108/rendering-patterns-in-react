8f0bbaacdc48cc146a48a5b4855cbaba4151f19d
Josué Hernández <joheca1998@outlook.com>
2025-03-04 18:45:20 -0600

    Implementando Control Props

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
+
+type ToogleProps = {
+  isToggle?: boolean;
+  onToggle?: (value: boolean) => void;
+}
+
+export const Toogle: React.FC<ToogleProps> = ({ isToggle = false, onToggle }) => {
+  const [internalToggle, setInternalToggle] = useState(isToggle);
+  
+  const toogle = () =>{
+    const newToggle = !internalToggle;
+    setInternalToggle(newToggle);
+    
+    if(onToggle){
+      onToggle(newToggle);
+    }
+  }
+  
+  return (
+    <button onClick={toogle}>
+      {internalToggle ? "ON ❤️‍🔥": "OFF 😦"}
+    </button>
+  );
+};
+
 export const ParentComponent = () => {
+  const [toogleState, setToggleState] = useState(false);
+
   return (
     <div>
+      <p>Toggle is {toogleState ? "ON ❤️‍🔥": "OFF 😦"} </p>
+      <Toogle isToggle={toogleState} onToggle={setToggleState}/>
     </div>
   );
 };
