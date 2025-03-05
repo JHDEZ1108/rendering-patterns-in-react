a7876a882d0a399a36ec6563d707e1746b62e79d
Josué Hernández <joheca1998@outlook.com>
2025-03-04 20:15:55 -0600

    Implementando Patron Compound

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,18 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Girafa">
+        🦒
+      </Tab>
+      <Tab label="Oso">
+        🐻
+      </Tab>
+      <Tab label="León">
+        🦁
+      </Tab>
+    </Tabs>
   );
 };
